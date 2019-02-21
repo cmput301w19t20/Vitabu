b428af20533e17af6e52f9d2be6ddb11dac4a827
aoladosu <oladosu@ualberta.ca>
2019-02-21 15:14:30 -0700

    added unit tests for getters and setters for user and book class

--- app/src/test/java/com/example/vitabu/VitabuUnitTest.java
+++ app/src/test/java/com/example/vitabu/VitabuUnitTest.java
@@ -2,6 +2,8 @@ package com.example.vitabu;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -10,8 +12,65 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class VitabuUnitTest {
+
+
+    // test book class
+    Book book = new Book();
+    book.setTitle("Vitabu");
+    book.setAuthor("Vitabu Inc");
+    book.setISBN(123456789);
+    book.setStatus("available");
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void bookTitle_isCorrect() {
+        assertEquals("Vitabu", book.getTitle());
+    }
+    @Test
+    public void bookAuthor_isCorrect() {
+        assertEquals("Vitabu Inc", book.getAuthor());
+    }
+    @Test
+    public void bookISBN_isCorrect() {
+        assertEquals(123456789, book.getISBN());
+    }
+    @Test
+    public void bookStatus_isCorrect() {
+        assertEquals("available", book.getStatus());
+    }
+
+
+    // test user class
+    User user = new Book();
+    user.setUserid("Vitabu User");
+    user.setLocation("Edmonton");
+    user.setBorrowRating(4);
+    user.setOwnerRating(3);
+    Date date = new Date();
+    user.setJoinDate(date);
+    user.setEmail("Vitabu@gmail.com");
+
+    @Test
+    public void userUserid_isCorrect() {
+        assertEquals("Vitabu User", user.getUserid());
+    }
+    @Test
+    public void userLocation_isCorrect() {
+        assertEquals("Edmonton", user.getLocation());
+    }
+    @Test
+    public void userBorrowRating_isCorrect() {
+        assertEquals(4, user.getBorrowRating());
+    }
+    @Test
+    public void userOwnerRating_isCorrect() {
+        assertEquals(3, user.getOwnerRating());
+    }
+    @Test
+    public void userJoinDate_isCorrect() {
+        assertEquals(date, user.getJoinDate());
+    }
+    @Test
+    public void userEmail_isCorrect() {
+        assertEquals("Vitabu@gmail.com", user.getEmail());
     }
 }
