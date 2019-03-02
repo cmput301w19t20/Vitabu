e948ff2df72f2f722ef7d03b11ff6017abda9080
jacobpaton <jacobpaton@shaw.ca>
2019-03-01 21:06:55 -0700

    Finished setMeetingActivity

--- app/src/main/java/com/example/vitabu/MainActivity.java
+++ app/src/main/java/com/example/vitabu/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_set_meeting);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
