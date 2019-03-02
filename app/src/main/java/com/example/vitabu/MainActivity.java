1eb1083a22db4056d30fae2b9900436c7764d6bc
jacobpaton <jacobpaton@shaw.ca>
2019-03-01 21:09:03 -0700

    Set the launch activity back to main

--- app/src/main/java/com/example/vitabu/MainActivity.java
+++ app/src/main/java/com/example/vitabu/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_meeting);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
