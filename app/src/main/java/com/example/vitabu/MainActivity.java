90a0e6874c56457722adc3ad485d4361492671d2
Arseniy Kouzmenkov <kouzmenk@ualberta.ca>
2019-03-05 16:55:10 -0700

    fixed all the merging. Time to integrate

--- app/src/main/java/com/example/vitabu/MainActivity.java
+++ app/src/main/java/com/example/vitabu/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(logTag, "Failed to sign in with email: " + email, task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Sign In failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
@@ -126,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(logTag, "Failed to create user with email: " + email, task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Sign Up failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
