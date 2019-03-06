207c44e6d77b0d6edbc4904ecdf8dcbc485e84c0
Arseniy Kouzmenkov <kouzmenk@ualberta.ca>
2019-03-05 21:22:31 -0700

    Added verification that the username does not exist.

--- app/src/main/java/com/example/vitabu/registerActivity.java
+++ app/src/main/java/com/example/vitabu/registerActivity.java
@@ -10,18 +10,26 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class registerActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;  //The firebase authorization handle.
-    private User usr;  //The user object handle.
-    private Location location; //Handle on the location object.
+    private User usr = new User();  //The user object handle.
+    private Location location = new Location(); //Handle on the location object.
     private String logTag = "registerActivity";
     private boolean signUpAttemptReturnValue;
+    private boolean usernameExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,10 @@ public class registerActivity extends AppCompatActivity {
 
     public void updateReturnValue(boolean value){
         signUpAttemptReturnValue = value;
+    }
+
+    public void updateUsernameExists(boolean value){
+        usernameExists = value;
     }
 
     public boolean attemptSignUp(final String email, String password){
@@ -61,6 +73,8 @@ public class registerActivity extends AppCompatActivity {
         return signUpAttemptReturnValue;
     }
 
+
+
     public void onRegisterClick(View view){
         String temp;
 
@@ -71,40 +85,64 @@ public class registerActivity extends AppCompatActivity {
         EditText province = (EditText) findViewById(R.id.register_province);
         EditText city = (EditText) findViewById(R.id.register_city);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
 
         if(username.getText().toString().length() < 1){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a full username.", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a full username.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        myRef.child("users").child(username.getText().toString())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user information
+                        if (dataSnapshot.exists()){
+                            updateUsernameExists(true);
+                        }
+                        else {
+                            updateUsernameExists(false);
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+        if (usernameExists) {
+            Toast.makeText(getApplicationContext(), "This Username already exists!", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(email.getText().toString().length() < 1){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a valid email.", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a valid email.", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(password.getText().toString().length() < 8){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a password that is more than 8 characters long.", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a password that is more than 8 characters long.", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(country.getText().toString().length() < 1){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a country.", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a country.", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(province.getText().toString().length() < 1){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a province..", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a province..", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(city.getText().toString().length() < 1){
-            Toast toast = Toast.makeText(getApplicationContext(), "Please, provide a city.", Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getApplicationContext(), "Please, provide a city.", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -112,12 +150,35 @@ public class registerActivity extends AppCompatActivity {
             return;
         }
         else {
-            Intent intent = new Intent();
             Toast.makeText(getApplicationContext(), "User Successfully registered, please sign in now.", Toast.LENGTH_LONG).show();
-
-            setResult(RESULT_OK, intent);
-            finish();
         }
+
+        usr.setUserName(username.getText().toString());
+        usr.setEmail(email.getText().toString());
+        location.setCountry(country.getText().toString());
+        location.setProvinceOrState(province.getText().toString());
+        location.setCity(city.getText().toString());
+        usr.setLocation(location);
+
+
+
+        myRef.child("users").child(usr.getUserName()).setValue(usr)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Added to the database successfully", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Did not add to the database successfully", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
 
     }
 }
