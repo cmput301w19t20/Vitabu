c72539bfe79f27049be657b86877e457965c29e0
Tristan_C <tristanc97@gmail.com>
2019-03-07 02:12:57 -0700

    Better integrated sign in/up screens, Added error checking for those screens, and changed User class to be more tightly integrated with Firebase

--- app/src/main/java/com/example/vitabu/registerActivity.java
+++ app/src/main/java/com/example/vitabu/registerActivity.java
@@ -16,108 +16,43 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONObject;
+
 public class registerActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;  //The firebase authorization handle.
-    private User usr = new User();  //The user object handle.
+    private User usr;  //The user object handle.
     private Location location = new Location(); //Handle on the location object.
     private String logTag = "registerActivity";
-    private boolean signUpAttemptReturnValue;
-    private boolean usernameExists;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        //Initialize the firebase authorization service
         auth = FirebaseAuth.getInstance();
     }
 
-    public void updateReturnValue(boolean value){
-        signUpAttemptReturnValue = value;
-    }
-
-    public void updateUsernameExists(boolean value){
-        usernameExists = value;
-    }
-
-    public boolean attemptSignUp(final String email, String password){
-
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(logTag, "Successfully created user with email: " + email);
-                            //FirebaseUser user = auth.getCurrentUser();
-                            //updateUI(user);
-                            updateReturnValue(true);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(logTag, "Failed to create user with email: " + email, task.getException());
-                            Toast.makeText(getApplicationContext(), "Sign Up failed. The user with this email may already exist.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                            updateReturnValue(false);
-                        }
-                    }
-                });
-        return signUpAttemptReturnValue;
-    }
-
-
 
     public void onRegisterClick(View view){
-        String temp;
 
-        EditText username = (EditText) findViewById(R.id.register_username);
-        EditText email = (EditText) findViewById(R.id.register_email);
-        EditText password = (EditText) findViewById(R.id.register_password);
-        EditText country = (EditText) findViewById(R.id.register_country);
-        EditText province = (EditText) findViewById(R.id.register_province);
-        EditText city = (EditText) findViewById(R.id.register_city);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
-
+        final EditText username = (EditText) findViewById(R.id.register_username);
+        final EditText email = (EditText) findViewById(R.id.register_email);
+        final EditText password = (EditText) findViewById(R.id.register_password);
+        final EditText country = (EditText) findViewById(R.id.register_country);
+        final EditText province = (EditText) findViewById(R.id.register_province);
+        final EditText city = (EditText) findViewById(R.id.register_city);
 
         if(username.getText().toString().length() < 1){
             Toast.makeText(getApplicationContext(), "Please, provide a full username.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        myRef.child("users").child(username.getText().toString())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // Get user information
-                        if (dataSnapshot.exists()){
-                            updateUsernameExists(true);
-                        }
-                        else {
-                            updateUsernameExists(false);
-                        }
-
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-        if (usernameExists) {
-            Toast.makeText(getApplicationContext(), "This Username already exists!", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -146,39 +81,81 @@ public class registerActivity extends AppCompatActivity {
             return;
         }
 
-        if (attemptSignUp(email.getText().toString(), password.getText().toString())){
-            return;
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "User Successfully registered, please sign in now.", Toast.LENGTH_LONG).show();
-        }
 
-        usr.setUserName(username.getText().toString());
-        usr.setEmail(email.getText().toString());
         location.setCountry(country.getText().toString());
         location.setProvinceOrState(province.getText().toString());
         location.setCity(city.getText().toString());
-        usr.setLocation(location);
+        String strUsername = username.getText().toString();
+        // Check if username alredy exists.
+        myRef.child("usernames").child(strUsername).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.d(logTag, "got data");
+                if (snapshot.exists()){
+                    Log.d(logTag, "uname taken");
+                    Toast.makeText(getApplicationContext(), "Username Is taken.", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    Log.d(logTag, "uname not taken");
+                    // Create new user.
+                    signUp(location, username.getText().toString(), email.getText().toString(), password.getText().toString());
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(logTag, "Database Error", databaseError.toException());
+            }
+        });
+
+
+    }
 
 
 
-        myRef.child("users").child(usr.getUserName()).setValue(usr)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+    public void signUp(final Location location, final String userName, final String email, final String password){
+        // Attempt to create user.
+        Log.d(logTag, "In signup");
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(getApplicationContext(), "Added to the database successfully", Toast.LENGTH_LONG).show();
+                    public void onSuccess(AuthResult authResult) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(logTag, "Successfully created user with email: " + email);
+                        usr = new User(location, auth.getCurrentUser());
+
+                        // TODO Deal with failed user account update.
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(userName)
+                                .build();
+
+                        usr.getFireBaseUser().updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.d(logTag, "User profile updated.");
+                                            usr.writeToDatabase();
+                                        }
+                                    }
+                                });
+
+                        Toast.makeText(getApplicationContext(), "User Successfully registered, please sign in now.", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent();
+                        setResult(RESULT_OK, intent);
+                        finish();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), "Did not add to the database successfully", Toast.LENGTH_LONG).show();
+                        // If sign in fails, display a message to the user.
+                        Log.w(logTag, "Failed to create user with email: " + email, e);
+                        Toast.makeText(getApplicationContext(), "That email has been taken.",
+                                Toast.LENGTH_SHORT).show();
                     }
                 });
-
-        Intent intent = new Intent();
-        setResult(RESULT_OK, intent);
-        finish();
-
     }
+
+
 }
