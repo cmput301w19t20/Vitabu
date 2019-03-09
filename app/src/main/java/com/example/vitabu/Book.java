35b98f9729bea3d8323d4055f9acbfabf0d23129
aoladosu <oladosu@ualberta.ca>
2019-03-08 17:13:09 -0700

    simple java doc for getters and setters for the book class

--- app/src/main/java/com/example/vitabu/Book.java
+++ app/src/main/java/com/example/vitabu/Book.java
@@ -9,11 +9,11 @@ package com.example.vitabu;
 public class Book {
     private String title;
     private String author;
-    private int ISBN;
+    private String ISBN;
     private String status;
     private User owner;
 
-    public Book(String title, String author, int ISBN, String status, User owner) {
+    public Book(String title, String author, String ISBN, String status, User owner) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
@@ -25,46 +25,73 @@ public class Book {
 
     }
 
+    /**
+     * @return returns the owner of the book. A string.
+     */
     public User getOwner() {
         return owner;
     }
 
+    /**
+     * @param owner the owner of the book. Is a user object.
+     */
     public void setOwner(User owner) {
         this.owner = owner;
     }
 
+    /**
+     * @return returns the title of the book. A string.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @param title the title of the book. A string.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return returns the author of the book. A string.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * @param author the author of the book. A string.
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
-    public int getISBN() {
+    /**
+     * @return returns the ISBN of the book
+     */
+    public String getISBN() {
         return ISBN;
     }
 
-    public void setISBN(int ISBN) {
+    /**
+     * @param ISBN the isbn of the book
+     */
+    public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
 
+    /**
+     * @return returns the status of the book
+     */
     public String getStatus() {
         return status;
     }
 
     /**
      * Can only set the status to one of the following: requested, accepted, borrowed, or available. Any other value throws an error.
-     * @param status
-     * @throws IllegalArgumentException
+     * @param status a string representing the status of the book. One of requested, accepted, borrowed, or available.
+     * @throws IllegalArgumentException if status is not one of requested, accepted, borrowed, or available, an error is thrown
      */
     public void setStatus(String status) throws IllegalArgumentException{
         if(status != "available" && status != "requested" && status != "accepted" && status != "borrowed") {
