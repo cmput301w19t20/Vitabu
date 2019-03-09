4beb3c516cad3ddff3f38eed57260109cff5c601
aoladosu <oladosu@ualberta.ca>
2019-03-08 19:14:12 -0700

    start making a recycler view for user reviews

--- app/src/main/java/com/example/vitabu/userReviewActivity.java
+++ app/src/main/java/com/example/vitabu/userReviewActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class userReviewActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +13,12 @@ public class userReviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_review);
 
+
+        // get user object from intent
         Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Gson gson = new Gson();
+        IntentJson passed = gson.fromJson(message, IntentJson.class);
+        final UserAbstract user = (UserAbstract) passed.getObject(0);
     }
 }
