d8634773df892b6ec8e88fd5f6243c0edd8ddce6
Arseniy Kouzmenkov <kouzmenk@ualberta.ca>
2019-03-10 16:00:22 -0600

    wrote some javadocs and added some minor changes

--- app/src/main/java/com/example/vitabu/browseBooksActivity.java
+++ app/src/main/java/com/example/vitabu/browseBooksActivity.java
@@ -1,3 +1,12 @@
+/*
+ * This file contains the activity that the user reaches after successfully authenticating. It also
+ * initializes all the fragments for the screens that are located in this activity. This activity also
+ * has a bottom navigation bar that lets the user move between the different fragments.
+ *
+ * Author: Jacob Paton
+ * Version: 1.2
+ * Outstanding Issues: ---
+ */
 package com.example.vitabu;
 
 import android.content.Intent;
@@ -19,6 +28,19 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
+/**
+ * This class contains all the fragments and the bottom navigation bar that creates the UI. The bottom
+ * navigation bar lets the user go between the different fragments in the UI.
+ *
+ * @author Jacob Paton
+ * @version 1.2
+ * @see AddBookFragment
+ * @see bookRequestsFragment
+ * @see bookStatusFragment
+ * @see BrowseBooksFragment
+ * @see OwnedBooksFragment
+ * @see NotificationsFragment
+ */
 public class browseBooksActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
     private Fragment requests;
@@ -28,6 +50,13 @@ public class browseBooksActivity extends AppCompatActivity {
     private Fragment ownedBooks;
     private LocalUser curUser;
 
+    /**
+     * This method is called when this activity gets called initially. It gets the information about
+     * the user that is currently using the app from the authentication activity. This method also
+     * initializes all the fragments used in the activity.
+     *
+     * @param savedInstanceState keeps track of the state that the screen was in when it was left.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,17 +84,30 @@ public class browseBooksActivity extends AppCompatActivity {
 
         fragmentManager.beginTransaction().replace(R.id.browse_books_frame, browseBooks).commit();
 
+        //Initializes the Bottom Navigation Bar
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.browse_books_bottom_nav);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.browse_books_bottom_nav_menu_browse);
     }
 
+    /**
+     * This method creates the menu (an app bar) in the top right of the screen.
+     *
+     * @param menu the menu that needs to be created.
+     * @return true if the options menu was created successfully and false otherwise.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.browse_books_appbar, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * This method will go to the activity that is selected from the menu (app bar).
+     *
+     * @param item the selected item in the app bar
+     * @return returns true if an item was clicked successfully and false otherwise.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -82,11 +124,24 @@ public class browseBooksActivity extends AppCompatActivity {
 
     //This function sends the onActivityResult(...) call to the fragment where that
     //other activity was called so that it can be processed and dealt with correctly.
+
+    /**
+     * This function sends the onActivityResult(...) call to the fragment from which that activity
+     * was called so that the activity result can be processed and dealt with correctly.
+     *
+     * @param requestCode the request code with which the activity that just returned was called.
+     * @param resultCode the result code of the activity (either a RESULT_OK or something else)
+     * @param data the intent that contains the data that was passed back successfully.
+     * @see AddBookFragment
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /**
+     * This listener enables the navigation between fragments using the bottom navigation bar.
+     */
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
