367c0b82739dc50ef5089c03ae24aefeb52d66ed
Arseniy Kouzmenkov <kouzmenk@ualberta.ca>
2019-03-09 20:06:24 -0700

    wrote some javadocs for some of the model classes (User and Book), added an incrementer for the books owned counter in the User class when adding a book. Fixed register activity to actually finish when done registering

--- app/src/main/java/com/example/vitabu/Book.java
+++ app/src/main/java/com/example/vitabu/Book.java
@@ -1,11 +1,22 @@
+/*
+ * This file contains the model class for books. This file is needed to store book information into
+ * the database.
+ *
+ * Author: Owen Randall
+ * Version: 1.3
+ * Outstanding Issues: No place to store the ID of the images stored in Firestore.
+ */
+
 package com.example.vitabu;
 
 
 import java.util.UUID;
 
 /**
- * @version 1.0
  * The Book object for Vitabu that encapsulates all the possible attributes of a book
+ *
+ * @version 1.3
+ * @author Owen Randall
  */
 public class Book {
     private String title;
@@ -16,14 +27,29 @@ public class Book {
     private String description;
     private String bookid;
 
+    /**
+     * This is the constructor that is used to instantiate the Book object with all the necessary
+     * regular parameters.
+     *
+     * @param title the title of the book.
+     * @param author the author of the book
+     * @param ISBN the ISBN of the book
+     * @param status the status of the book (borrowed, available, requested, accepted).
+     * @param ownerName Username of the owner of the book.
+     */
     public Book(String title, String author, String ISBN, String status, String ownerName) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
         setStatus(status);
         this.ownerName = ownerName;
+        bookid = UUID.randomUUID().toString();
     }
 
+    /**
+     * This is the empty default constructor that is necessary for Firebase Database. We also use it
+     * when building up the class using the setters alone.
+     */
     public Book(){
         bookid = UUID.randomUUID().toString();
     }
@@ -74,8 +100,9 @@ public class Book {
 
 
     /**
-     * Can only set the status to one of the following: requested, accepted, borrowed, or available. Any other value throws an error.
-     * @param status
+     * Can only set the status to one of the following: requested, accepted, borrowed, or available.
+     * Any other value throws an error.
+     * @param status the value to which status needs to be set to.
      * @throws IllegalArgumentException
      */
     public void setStatus(String status) throws IllegalArgumentException{
