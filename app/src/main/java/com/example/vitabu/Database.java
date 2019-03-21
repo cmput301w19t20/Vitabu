ed51a8119baf4b193df5903d4923dbd01896601d
Owen Randall <davidowe@ualberta.ca>
2019-03-21 12:51:26 -0600

    Still trying to merge

--- app/src/main/java/com/example/vitabu/Database.java
+++ app/src/main/java/com/example/vitabu/Database.java
@@ -1,5 +1,6 @@
 package com.example.vitabu;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -7,7 +8,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,28 +18,35 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 /**
- * Singleton Class to encapsulate all logic for interacting with the database. All methods in this
+ * Singleton Class to encapsulate all logic for interacting with the database.
+ *
+ * All methods in this
  * class that are NOT constructors will take 2 Runnable objects as their first 2 arguments.
- * These arguments will be the success and fail callbacks respectively.
+ * These arguments will be the success and fail callbacks respectively.  All methods should be able
+ * to handle null being passed as callbacks without throwing an exception.
+ *
+ * If a value needs to be returned to the user, It should be saved, as a private variable within
+ * this class, then returned useing a getter that is to be called ONLY from within the success
+ * callback.
  *
  * An example of how to convert methods to a Runnable is shown below.
  * {@code
-          public void testMethod(String arg1, int arg2){
-              Log.d("TEST", "In callback function. arg1 = " + arg1 + " arg2 = " + arg2);
-          }
+public void testMethod(String arg1, int arg2){
+Log.d("TEST", "In callback function. arg1 = " + arg1 + " arg2 = " + arg2);
+}
 
-          ...
+...
 
-          public void someMethod(){
-              Runnable testRunnable = new Runnable() {
-                   @Override
-                   public void run() {
-                       testMethod(arg1, arg2);
-                   }
-               };
-               Database database = Database.getInstance()
-               database.testCallback(testRunnable);
-           }
+public void someMethod(){
+Runnable testRunnable = new Runnable() {
+@Override
+public void run() {
+testMethod(arg1, arg2);
+}
+};
+Database database = Database.getInstance()
+database.testCallback(testRunnable);
+}
 }
  *
  *
@@ -54,11 +61,15 @@ public class Database {
     private DatabaseReference rootReference;
     private static final Database ourInstance = new Database();
     private ArrayList<Book> searchBooksReturnValue;
+    private ArrayList<BorrowRecord> getBorrowRecordsByBookidReturnValue;
+    private User fetchUserReturnValue;
     private Iterable<DataSnapshot> queryResult;
+
+    // TODO Move all other database accesses to this class.
 
     /**
      * Method to get reference to the singleton object.
-     * @return
+     * @return Database
      */
     public static Database getInstance() {
         return ourInstance;
@@ -71,16 +82,6 @@ public class Database {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         rootReference = database.getReference();
-    }
-
-    /**
-     * Returns the username of the current user
-     * @return the userName of the current user
-     */
-    public String getCurUserName(){
-        FirebaseUser firebaseUser = auth.getCurrentUser();
-        String userName = firebaseUser.getDisplayName();
-        return userName;
     }
 
     /**
@@ -126,9 +127,9 @@ public class Database {
      *
      * @param successCallback Runnable to call on success.
      * @param failCallback Runnable to call on fail.
-     * @param email
-     * @param password
-     * @param userName
+     * @param email String
+     * @param password String
+     * @param userName String
      * @param location Location object
      */
     public void createUser(final Runnable successCallback, final Runnable failCallback, final String email, final String password, final String userName, final Location location){
@@ -156,7 +157,8 @@ public class Database {
                                         }
                                         else{
                                             Log.d(logTag, "User Profile update Failed.  This is bad.");
-                                            failCallback.run();
+                                            if (failCallback != null)
+                                                failCallback.run();
                                         }
                                     }
                                 });
@@ -167,7 +169,8 @@ public class Database {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(logTag, "Failed to create user with email: " + email, e);
-                        failCallback.run();
+                        if (failCallback != null)
+                            failCallback.run();
                     }
                 });
     }
@@ -192,29 +195,44 @@ public class Database {
      * @param username String username to be checked.
      */
     public void checkUsernameAvailability(final Runnable successCallback, final Runnable failCallback, final String username){
-            rootReference.child("usernames").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+        rootReference.child("usernames").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 Log.d(logTag, "Got username " + username + " Data from the database");
                 if (snapshot.exists()){
                     // Username is taken
-                    failCallback.run();
+                    if (failCallback != null)
+                        failCallback.run();
 
                 }
                 else{
                     // Username available.
-                    successCallback.run();
+                    if (successCallback != null)
+                        successCallback.run();
                 }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.d(logTag, "Database Error", databaseError.toException());
-                failCallback.run();
+                if (failCallback != null)
+                    failCallback.run();
             }
         });
 
     }
 
+    /**
+     * Searches database for books based on the provided search criteria. The list of books matching
+     * the criteria can be obtained by calling the method getSearchBooksReturnValue() in the
+     * successCallback method.
+     *
+     * @param successCallback Runnable to be called back on successful search.
+     * @param failCallback Runnable to be called if there is a database error.
+     * @param author String
+     * @param title String
+     * @param isbn String
+     * @param kwords String
+     */
     public void searchBooks(final Runnable successCallback, final Runnable failCallback, final String author, final String title, final String isbn, final String kwords){
         final ArrayList<Book> bookList = new ArrayList<>();
         String initialSearchValue = "";
@@ -242,12 +260,13 @@ public class Database {
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         Log.d(logTag, dataSnapshot.toString());
                         for (DataSnapshot subSnapshot : dataSnapshot.getChildren()) {
+
                             Book curBook = subSnapshot.getValue(Book.class);
                             Log.d(logTag, curBook.getBookid());
                             if ((title.equals("") || curBook.getTitle().equals(title)) &&
                                     (author.equals("") || curBook.getAuthor().equals(author)) &&
                                     (isbn.equals("") || curBook.getISBN().equals(isbn)) &&
-                                    (kwords.equals("") || curBook.getDescription().contains(kwords) )&&
+                                    (kwords.equals("") || curBook.getDescription().equals("") || curBook.getDescription().contains(kwords) )&&
                                     (author.equals("") || curBook.getAuthor().equals(author))
                             ) {
                                 bookList.add(curBook);
@@ -255,7 +274,8 @@ public class Database {
 
                         }
                         searchBooksReturnValue = bookList;
-                        successCallback.run();
+                        if (successCallback != null)
+                            successCallback.run();
 
 
                     }
@@ -263,11 +283,169 @@ public class Database {
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                         Log.d(logTag, "Uhh Failed to get books from database...", databaseError.toException());
-                        failCallback.run();
+                        if (failCallback != null)
+                            failCallback.run();
                     }
                 });
 
 
+    }
+
+    /**
+     *
+     * @return ArrayList of books matching the search.
+     */
+    public ArrayList<Book> getSearchBooksReturnValue(){
+        ArrayList<Book> ret = searchBooksReturnValue;
+        searchBooksReturnValue = null;
+        return ret;
+    }
+
+    /**
+     * Gets all borrowrecords that have matching Book id.
+     *
+     * NOTE: this method currently sets a permanant listener for datachange! Meaning it will
+     * continually call the succsess callback passed to it whenever the database changes!
+     *
+     * @param successCallback Runnable
+     * @param failCallback Runnable
+     * @param bookid String
+     */
+    public void findBorrowRecordsByBookid(final Runnable successCallback, final Runnable failCallback, final String bookid) {
+        getBorrowRecordsByBookidReturnValue = new ArrayList<>();
+        rootReference.child("borrowrecords").orderByChild("bookid").equalTo(bookid).addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot subSnapshot : snapshot.getChildren()) {
+                            getBorrowRecordsByBookidReturnValue.add(subSnapshot.getValue(BorrowRecord.class));
+                        }
+                        if (successCallback != null)
+                            successCallback.run();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError e) {
+                        Log.d(logTag, "Database error in getBorrowRecordsByBookid:", e.toException());
+                        if (failCallback != null)
+                            failCallback.run();
+                    }
+                });
+    }
+
+    /**
+     * Used to get the ArrayList of borrowRecords that was generated by findBorrowRecordsByBookId()
+     * @return Arraylist<BorrowRecord>
+     */
+    public ArrayList<BorrowRecord> getFindBorrowRecordsByBookidReturnValue(){
+        ArrayList<BorrowRecord> ret = getBorrowRecordsByBookidReturnValue;
+        getBorrowRecordsByBookidReturnValue = null;
+        return ret;
+    }
+
+    /**
+     * Fetches a user from database.
+     * @param successCallback Runnable
+     * @param failCallback Runnable
+     * @param username User
+     */
+    public void fetchUser(final Runnable successCallback, final Runnable failCallback, String username) {
+        rootReference.child("users").child(username).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        fetchUserReturnValue = dataSnapshot.getValue(User.class);
+                        if (successCallback != null)
+                            successCallback.run();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.d(logTag, "Database error", databaseError.toException());
+                        if (failCallback != null)
+                            failCallback.run();
+                    }
+                }
+        );
+    }
+
+    /**
+     * Returns the user that was fetched by fetchUser()
+     * This method is only to be called from within the success callback passed to fetchUser()
+     *
+     * @return User
+     */
+    public User getFetchUserReturnValue(){
+        User ret = fetchUserReturnValue;
+        fetchUserReturnValue = null;
+        return ret;
+    }
+
+    /**
+     * Accepts a borrow request.
+     *
+     * @param successCallback Runnable
+     * @param failCallback Runnable
+     * @param record BorrowRecord
+     */
+    public void acceptBorrowRequest(final Runnable successCallback, final Runnable failCallback, final BorrowRecord record){
+        // update accepted borrowRecord in database.
+        rootReference.child("borrowrecords").child(record.getRecordid()).setValue(record);
+
+        // Delete all other not accepted requests for the same book and user from the database.
+        rootReference.child("borrowrecords").orderByChild("ownerName").equalTo(record.getOwnerName()).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot subSnapshot : snapshot.getChildren()) {
+                            String curbookid = (String) subSnapshot.child("bookid").getValue();
+                            boolean curRecordApproved = (boolean) subSnapshot.child("approved").getValue();
+                            String curRecordId = (String) subSnapshot.child("recordid").getValue();
+                            if (curbookid.equals(record.getBookid()) && ! curRecordApproved && ! curRecordId.equals(record.getRecordid())) {
+                                // Delete current record from the database.
+                                Log.d(logTag, "Removing borrow record "+ subSnapshot.getKey());
+                                rootReference.child("borrowrecords").child(subSnapshot.getKey()).removeValue();
+                            }
+                        }
+                        if (successCallback != null)
+                            successCallback.run();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError e) {
+                        if (failCallback != null)
+                            failCallback.run();
+                    }
+                });
+
+    }
+
+    /**
+     * Denys a borrow record.
+     *
+     * @param successCallback Runnable
+     * @param failCallback Runnable
+     * @param record BorrowRecord
+     */
+    public void denyBorrowRequest(final Runnable successCallback, final Runnable failCallback, final BorrowRecord record){
+        // Remove borrowRecord from database.
+        rootReference.child("borrowrecords").child(record.getRecordid()).removeValue(new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    Log.d(logTag, "Successfully removed record " + record.getRecordid());
+                    if (successCallback != null)
+                        successCallback.run();
+                }
+                else{
+                    Log.d(logTag, "failed to remove record " + record.getRecordid());
+                    if (failCallback != null)
+                        failCallback.run();
+                }
+            }
+        });
+
+        // TODO Remove notification from database as well!
     }
 
     /**
@@ -296,14 +474,6 @@ public class Database {
 
     public Iterable<DataSnapshot> getQueryResult() {
         return queryResult;
-    }
-
-    public ArrayList<Book> getSearchBooksReturnValue(){
-        return searchBooksReturnValue;
-    }
-
-    public DatabaseReference getRootReference() {
-        return rootReference;
     }
 
 }
