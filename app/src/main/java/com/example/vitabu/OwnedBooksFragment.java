36e02869492ef16dac6417ce05f8593473450c2a
Owen Randall <davidowe@ualberta.ca>
2019-03-22 17:54:23 -0600

    Fixed some errors and added navigation to approprate pages from ownedBooksFragment

--- app/src/main/java/com/example/vitabu/OwnedBooksFragment.java
+++ app/src/main/java/com/example/vitabu/OwnedBooksFragment.java
@@ -172,10 +172,14 @@ public class OwnedBooksFragment extends Fragment implements AdapterView.OnItemSe
                                     if (bookids.size() == 0) {
                                         String bid = b.getBookid();
                                         bookids.add(bid);
+                                        ArrayList<Book> copy = new ArrayList<>();
                                         for (Book book : books) {
                                             if (book.getBookid().equals(bid)) {
-                                                books.remove(book);
+                                                copy.add(book);
                                             }
+                                        }
+                                        for (Book book: copy){
+                                            books.remove(book);
                                         }
                                     }
                                     books.add(b);
@@ -277,10 +281,26 @@ public class OwnedBooksFragment extends Fragment implements AdapterView.OnItemSe
 //        TODO: Opens book info activity
         Toast.makeText(this.getActivity(), "You clicked " + recyclerViewAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
         Book book = recyclerViewAdapter.getItem(position);
-        Intent intent = new Intent(this.getContext(), bookEditActivity.class);
         Gson gson = new Gson();
-        String message = gson.toJson(recyclerViewAdapter.getItem(position));
-        intent.putExtra(MainActivity.BOOK_MESSAGE, message);
+        Intent intent = new Intent();
+        if(book.getStatus().equals("available")) {
+            intent = new Intent(this.getContext(), bookEditActivity.class);
+            String message = gson.toJson(book);
+            intent.putExtra(MainActivity.BOOK_MESSAGE, message);
+        }else if(book.getStatus().equals("requested")){
+            intent = new Intent(this.getContext(), acceptBookRequestActivity.class);
+            String message = gson.toJson(book);
+            intent.putExtra(MainActivity.BOOK_MESSAGE, message);
+        }else if(book.getStatus().equals("accepted")) {
+            //TODO: change activity to the new tradeoff activity
+            intent = new Intent(this.getContext(), acceptBookRequestActivity.class);
+            String message = gson.toJson(book);
+            intent.putExtra(MainActivity.BOOK_MESSAGE, message);
+        }else if(book.getStatus().equals("borrowed")) {
+            intent = new Intent(this.getContext(), returnBookActivity.class);
+            String message = gson.toJson(book);
+            intent.putExtra(MainActivity.BOOK_MESSAGE, message);
+        }
         startActivity(intent);
     }
 }
