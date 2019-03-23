1d5f6e0080e11724beecdef510a42e08075bda8f
Katherine Richards <kar3@ualberta.ca>
2019-03-23 07:23:36 -0600

    added UI to display search results searchBookResultsActivity

--- app/src/main/java/com/example/vitabu/searchBookResultsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/vitabu/searchBookResultsRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.vitabu;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,77 +9,83 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class searchBookResultsRecyclerViewAdapter extends RecyclerView.Adapter<searchBookResultsRecyclerViewAdapter.ViewHolder> {
 
     private ArrayList<Book> mData;
-    private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
+    private OnItemClickListener mClickListener;
+    private Context context;
 
-    // data is passed into the constructor
-    searchBookResultsRecyclerViewAdapter(Context context, ArrayList<Book> data) {
-        this.mInflater = LayoutInflater.from(context);
-        this.mData = data;
+    // activity will have to implement these interface methods for an onclick event inside a row
+    public interface OnItemClickListener {
+        void onItemClick(int position);
     }
 
-    // inflates the layout from xml when needed
+    // set up the click listener
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mClickListener = listener;
+    }
+
+    // viewholder class declaration
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView author;
+        public TextView status;
+
+        // constructor
+        public ViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            title = itemView.findViewById(R.id.search_books_book_title);
+            author = itemView.findViewById(R.id.search_books_book_author);
+            status = itemView.findViewById(R.id.search_books_book_status);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+
+
+    // set the data list to the recycler view
+    public searchBookResultsRecyclerViewAdapter(ArrayList<Book> data) {
+        mData = data;
+    }
+
+    // helper method for returning row id of list item clicked
+    Book getItem(int id) { return mData.get(id); }
+
+    // getter method to access context of adapter
+    Context getContext() {return context;}
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.content_search_book_results, parent, false);
-        return new searchBookResultsRecyclerViewAdapter.ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_search_book_results, parent, false);
+        context = parent.getContext();
+
+        return new ViewHolder(view, mClickListener);
     }
 
-    // binds the data to the fields in each row
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Book book = mData.get(position);
-        String title = book.getTitle();
-        String author = book.getAuthor();
-        String status = book.getStatus();
-
-        holder.title.setText(title);
-        holder.author.setText(author);
-        holder.status.setText(status);
+        holder.title.setText(book.getTitle());
+        holder.author.setText(book.getAuthor());
+        holder.status.setText(book.getStatus());
     }
 
-    // Returns total number of rows
     @Override
     public int getItemCount() {
         return mData.size();
     }
 
 
-    // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView title, author, status;
-
-        ViewHolder(View itemView) {
-            super(itemView);
-            title = itemView.findViewById(R.id.search_books_book_title);
-            author = itemView.findViewById(R.id.search_books_book_author);
-            status = itemView.findViewById(R.id.search_books_book_status);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
-    }
-
-    // convenience method for getting data at click position
-    Book getItem(int id) {
-        return mData.get(id);
-    }
-
-    // allows clicks events to be caught
-    void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
-    }
 }
